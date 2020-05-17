7c40ab7648f6ce1ff2def171326e352824357867
Tal Tarablus <45769558+taltara@users.noreply.github.com>
2020-05-17 15:50:28 +0300

    Update NavBar.jsx

--- cmps/NavBar.jsx
+++ cmps/NavBar.jsx
@@ -7,7 +7,7 @@ export function NavBar(props) {
     return (
 
         <nav className="app-nav flex space-between align-center">
-            <span><NavLink exact to='/'><span className="logo-text">Miss</span><img src="../assets/img/logo.png" className="main-logo" /><span className="logo-text">Book</span></NavLink></span>
+            <span><NavLink exact to='/'><span className="logo-text">Miss</span><img src="assets/img/logo.png" className="main-logo" /><span className="logo-text">Book</span></NavLink></span>
             <UserMsg></UserMsg>
             <span className="nav-links-span flex align-center space-between">
                 <p className="navlink-collection navlink"><NavLink exact to='/books'>Collection</NavLink></p>
