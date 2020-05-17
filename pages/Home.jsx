86f406c8c9ac671e092ba5660f4ce0a2c0bc362e
Tal Tarablus <45769558+taltara@users.noreply.github.com>
2020-05-17 15:43:29 +0300

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -6,7 +6,7 @@ export function Home(props) {
         <section className="home-section container flex column align-center space-center">
             <p className="home-header">Welcome To Miss Book!</p>
             <p className="home-secondary">Your books are our speciality!</p>
-            <img src="../assets/img/home.JPG"/>
+            <img src="../assets/img/home.jpg"/>
             <p className="home-text">You can easily browse our books collection, filter by title, 
                 authors and even price to find your next big adventure!
                 Be sure to leave a review and share with everyone you love the passion that we provide!</p>
